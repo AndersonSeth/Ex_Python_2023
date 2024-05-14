2adec0090cf70feb34635aa7d2253124110c2306
Anderson Chagas da Silva <anderson.chga@gmail.com>
2024-05-14 15:47:52 -0300

    Commit

---
+++ DAM/FLASK/Flask Post/Estudando_flask_post.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+pessoas = [
+    {"nome": "Paulo", "sexo": "M", "cabelo": "loiro"},
+    {"nome": "Maria", "sexo": "F", "cabelo": "preto"},
+    {"nome": "Fernanda", "sexo": "F", "cabelo": "ruivo"},
+    {"nome": "José", "sexo": "M", "cabelo": "careca"}
+]
+
+@app.route("/pessoas", methods=["GET"])
+def consulta_pessoas():
+    return(pessoas)
+
+@app.route("/pessoa", methods = ["POST"])
+def cadastrar():
+    pessoa = request.json
+    pessoas.append(pessoa)
+    return jsonify(pessoas)
+
+if __name__ == "__main__":
+    app.run(host = "localhost", port = 5000)
